81b9331bedef85c56889a4461eef712a107fc38d
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-20 16:41:07 -0300

    Order, Instant, ISO 8601 - part 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -11,20 +11,24 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "clien_id")
 	private User client;
-	
+
 	public Order() {
 	}
 
@@ -53,7 +57,7 @@ public class Order implements Serializable {
 	public User getClient() {
 		return client;
 	}
-	
+
 	public void setClient(User client) {
 		this.client = client;
 	}
@@ -82,5 +86,5 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
