7ddbca429e70be07150dfd38c4bfcc758d5b2907
Pedro Henrique Sobrinho Gamarra da Silva <Pedrohhouro@gmail.com>
2020-10-22 13:57:53 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceExceptionHandler.java
@@ -25,4 +25,15 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	
 	}
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
+
+		String error = "DataBase ERROR";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	
+	}
+	
+	
 }
